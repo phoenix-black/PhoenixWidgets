3b42f2ac696bd1118fdfae14e9ddaf517337edf4
Praba <prabalinux@gmail.com>
2018-02-28 16:21:56 +0530

    added logfile support (commented in ILog.java)

--- app/src/main/java/com/blackphoenix/phoenixwidgets/ILog.java
+++ app/src/main/java/com/blackphoenix/phoenixwidgets/ILog.java
@@ -3,8 +3,9 @@ package com.blackphoenix.phoenixwidgets;
 import android.util.Log;
 
 /**
- * Created by shree on 1/2/2018.
+ * Created by Praba on 1/2/2018.
  */
+
 public class ILog {
 
     public static void print(String title, String message){
@@ -14,5 +15,28 @@ public class ILog {
     public static void error(String errorTitle, String errorMessage){
         Log.e(""+errorTitle,""+errorMessage);
     }
+
+    /*public void appendLog(String mytag, String value, String filename) {
+        File logFile = new File("sdcard/" + filename + ".txt");
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        try {
+            //BufferedWriter for performance, true to set append to file flag
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            String con=mytag + "\t" + value;
+            buf.append(con);
+            buf.newLine();
+            buf.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }*/
 
 }
